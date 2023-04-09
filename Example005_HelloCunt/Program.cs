0d8ee1c516d513be4b02ab495dd9c184cc31fa61
e1win <e1win@mail.ru>
2023-04-09 13:47:44 +0300

    add Example 005 "if/else"

---
+++ Example005_HelloCunt/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name");
+string  username = Console.ReadLine();
+if(username.ToLower() == "e1win")
+{
+    Console.WriteLine("Let me give you a blowjob!");
+}
+else
+{
+    Console.Write("Hello, Cunt! Or i must call you ");
+    Console.WriteLine(username);
+}
