9124293b4401c673f34a21ab083f787b8eebb74c
e1win <e1win@mail.ru>
2023-05-11 21:44:29 +0300

    added info about replace cycle "for"

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -35,18 +35,76 @@
 
 // Console.WriteLine(year);
 
-string Method4(int count, string text)
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//         while (i < count)
+//         {
+//             result = result + text;
+//             i++;
+//         }
+//         return result;
+// }
+
+// string res = Method4(10, "qwertyText");
+// Console.WriteLine(res);
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//         for (i = 0; i < count; i++)
+//         {
+//             result = result + text;
+//         }
+//         return result;
+// }
+
+// string res = Method4(10, "qwertyText");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3]  // r
+
+string Replace(string text, char oldValue, char newValue)
 {
-    int i = 0;
     string result = String.Empty;
 
-        while (i < count)
-        {
-            result = result + text;
-            i++;
-        }
-        return result;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
 }
 
-string res = Method4(10, "qwertyText");
-Console.WriteLine(res);
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
