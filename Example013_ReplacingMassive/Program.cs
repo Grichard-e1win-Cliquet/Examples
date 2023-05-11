ea024c5789af04c7341747596d04b683ba0fd1e1
e1win <e1win@mail.ru>
2023-05-11 22:15:52 +0300

    added variation from max to min in Example013

--- Example013_ReplacingMassive/Program.cs
+++ Example013_ReplacingMassive/Program.cs
@@ -1,4 +1,42 @@
-﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+﻿Console.Clear();
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
+
+
+int[] arr = { 4, 52, 95, 412, 3, 25, 61, 27, 10, 1};
 
 void PrintArray(int[] array)
 {
@@ -15,16 +53,16 @@ void SelectionSort(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if(array[j] < array[minPosition]) minPosition = j;
+            if(array[j] > array[maxPosition]) maxPosition = j;
         }
 
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 }
 
